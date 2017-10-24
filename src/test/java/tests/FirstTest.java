22f8871b3882c8993023ae87d2229751339de9a1
serqbond <serqbond@gmail.com>
2017-10-24 17:27:33 +0300

    add description

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import org.junit.Test;
 import uicontext.googlesearchcontext.GoogleSearchContext;
@@ -11,6 +12,7 @@ public class FirstTest extends BaseTest {
 
     @Test
     @DisplayName("Search for pear test")
+    @Description("In this cool test we will test only cool features")
     public void searchForPearTest(){
     new GoogleSearchContext(driver)
             .opensTheSearchApp(baseUrl)
