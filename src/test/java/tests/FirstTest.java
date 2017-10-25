0ab3e5a76c89d4c133bb8ae4340a785c21a246a2
serqbond <serqbond@gmail.com>
2017-10-25 13:35:56 +0300

    try it

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -3,18 +3,16 @@ package tests;
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import org.junit.Test;
-import ru.yandex.qatools.allure.annotations.Title;
 import uicontext.googlesearchcontext.GoogleSearchContext;
 import io.qameta.allure.junit4.DisplayName;
 
-@Title("First Test")
-@Feature("Test Google search")
+@Feature("Boo")
+@io.qameta.allure.junit4.Tag("Bababoom")
 public class FirstTest extends BaseTest {
 
-    @Test
+    @Test()
     @DisplayName("Search for pear test")
     @Description("In this cool test we will test only cool features")
-    @Title("First pear test")
     public void searchForPearTest(){
     new GoogleSearchContext(driver)
             .opensTheSearchApp(baseUrl)
